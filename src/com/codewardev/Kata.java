7cec9b5e71abb063bf4d4019749d6aa00390fc29
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-11 15:52:01 +1100

    refined working cut

--- src/com/codewardev/Kata.java
+++ src/com/codewardev/Kata.java
@@ -6,11 +6,7 @@ public class Kata {
 
 	public static double findUniq(double[] arr) {
 		Arrays.sort(arr);
-		if(arr[0] != arr[1]) {
-			return arr[0];
-		} else {
-			return arr[arr.length-1];
-		}
+		return arr[0] != arr[1]?arr[0]:arr[arr.length-1];
 	}
 
 }
